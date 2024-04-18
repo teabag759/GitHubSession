7d4c98ab2a064c7ce958dde69ff10e57efa1e417
Lee Sohyeon <teabag759@gmail.com>
2024-04-18 19:59:32 +0900

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by 이소현 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
