a27430d79a87bd547cdd807bd6e2636af7f79f60
Lee Sohyeon <teabag759@gmail.com>
2024-04-18 20:29:48 +0900

    first commit

--- GitHub/ContentView.swift
+++ GitHub/ContentView.swift
@@ -8,7 +8,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Soy!")
         }
         .padding()
     }
